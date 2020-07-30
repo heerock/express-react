7a8cbd8fdee7c95e00d2e1b99479ec3cca46fcf1
heerock <heerock93@gmail.com>
2020-07-30 16:08:58 +0900

    react is my friend

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Customer from './components/Customer';
 
+const customer = {
+  'name': '차주훈',
+  'birthday': '930517',
+  'gender': '남자',
+  'job': '백수'
+}
 
 class App extends Component {
   render() {
     return (
-      <div className="gray-background">
-        <img src={logo} lat="logo" />
-        <h2>Let's develop management System</h2>
-      </div>
+      <Customer
+        name={customer.name}
+        birthday={customer.birthday}
+        gender={customer.gender}
+        job={customer.job}
+      />
 
 
     )
