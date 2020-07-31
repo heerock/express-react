580a649fa234924eed43201db248744985aca785
heerock <heerock93@gmail.com>
2020-07-31 19:38:57 +0900

    File Upload Status

--- server.js
+++ server.js
@@ -20,6 +20,9 @@ const conn = mysql.createConnection({
 });
 conn.connect();
 
+const multer = require('multer');
+const upload = multer({ dest: './upload' })
+
 app.get('/api/customers', (req, res) => {
     conn.query(
         "SELECT * FROM CUSTOMER",
@@ -29,5 +32,22 @@ app.get('/api/customers', (req, res) => {
     );
 });
 
+app.use('/image', express.static('./upload'));
+
+app.post('/api/customers', upload.single('image'), (req, res) => {
+    let sql = 'INSERT INTO CUSTOMER VALUES (null, ?, ?, ?, ?, ?)';
+    let image = '/image/' + req.file.filename;
+
+    let name = req.body.name;
+    let birthday = req.body.birthday;
+    let gender = req.body.gender;
+    let job = req.body.job;
+    let params = [image, name, birthday, gender, job];
+
+    conn.query(sql, params, (err, rows, fileds) => {
+        res.send(rows);
+    })
+
+});
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
